d27f4a471dafe48dab0fde7c5998da2c400d7d86
Vladislav A <validator1@yandex.ru>
2021-09-13 20:37:48 +0300

    made in the first approximation

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,26 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog1")
     private Animal animal;
+
+    // в задании написано:  Создайте на основе этого класса компонент. Создаем:
+    @Autowired
+    private Dog dog;
+
+    @Autowired
+    private Timer timer;
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +28,6 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+        System.out.println("--- VL: timer = " + timer.getTime());
     }
 }
